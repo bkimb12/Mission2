9ff2e0246b07439eb3385d4fa1ca862d38ac8806
bkimb12 <braedonk@byu.edu>
2022-01-19 23:15:12 -0700

    commit to mission3branch

---
+++ Mission2/Models/CalcModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission2.Models
+{
+    public class CalcModel
+    {
+        [Range(0, 100)]
+        public int Assign { get; set; }
+
+        [Range(0, 100)]
+        public int Group { get; set; }
+
+        [Range(0, 100)]
+        public int Quiz { get; set; }
+
+        [Range(0, 100)]
+        public int Ex { get; set; }
+
+        [Range(0, 100)]
+        public int Intex { get; set; }
+    }
+}
